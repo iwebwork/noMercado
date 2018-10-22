f680980ce0e41323b9178451e80061d094da2311
iwebwork <iwebwork@gmail.com>
2018-10-21 23:51:04 -0300

    Começou a listagem de produtos, mas ainda sem sucesso

--- app/appcorporativo/js/produtos.js
+++ app/appcorporativo/js/produtos.js
@@ -1,23 +1,53 @@
-function listagemProdutos(){
+
+
+/* global firebase, dbRef, codigoEstabelecimento, username */
+
+
     
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          //Pequei o Uid do usuario, agr e listar seus dados com ele
-          var id = firebase.auth().currentUser.uid;
-          
-          var playersRef = firebase.database().ref("UsuariosCorporativos/" + id);
-          playersRef.on("child_added", snap => {
-                var user = new Object();
-                user = snap.val();
-                
-                alert(user);
+                //Peguei o Uid do usuario, agora e listar seus dados com ele
+                id = firebase.auth().currentUser.uid;
+		  
+		firebase.database().ref('/UsuariosCorporativos/' + id).once('value').then(function(snapshot) {
+                    var username = (snapshot.val() && snapshot.val().nome); //esse .nome representa o atributo que você deseja buscar
+                     //  no banco de dados
+                        // ...
+                    
+                    alert("Nome: "+username);
+                    
+                });
 
-          });
-          
+                firebase.database().ref('/UsuariosCorporativos/' + id).once('value').then(function(snapshot) {
+                    var email = (snapshot.val() && snapshot.val().email);//buscando o email
+                    // ...
+                    
+                    //alert("Email: "+email);
+                });
+                
+                firebase.database().ref('/UsuariosCorporativos/' + id).once('value').then(function(snapshot) {
+                    var codigoEstabelecimento = (snapshot.val() && snapshot.val().codigoEstabelecimento);//buscando o código
+                        // ...
+                    
+                    alert("codigoEstabelecimento: "+ codigoEstabelecimento);
+                    
+                });
+                
+                
+                listagemProdutos(username, codigoEstabelecimento);
           
         } else {
             alert("Não funcionou");
         }
     });
     
+
+
+function listagemProdutos(nome,codigo){
+                
+        alert("Nome: " + nome + " Codigo: " + codigo);
+    
 }
+
+
+
